3bf213a2102908d82a224d7d9ac8a341562216ce
josiahalora-beep <josiahalora@gmail.com>
2026-09-20 17:57:27 -0400

    Import Bukkit world types for visible combat

--- server/plugins-src/EraCore/src/main/java/dev/jorel/eracore/SimWorldDirector.java
+++ server/plugins-src/EraCore/src/main/java/dev/jorel/eracore/SimWorldDirector.java
@@ -1,6 +1,8 @@
 package dev.jorel.eracore;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;

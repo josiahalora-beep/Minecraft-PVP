a1462871f869d07b8861dcb5046f40e7c1763c10
josiahalora-beep <josiahalora@gmail.com>
2026-09-22 20:58:20 -0400

    Fix cold-memory filename filter import

--- server/plugins-src/EraCore/src/main/java/dev/jorel/eracore/SimWorldDirector.java
+++ server/plugins-src/EraCore/src/main/java/dev/jorel/eracore/SimWorldDirector.java
@@ -16,6 +16,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
